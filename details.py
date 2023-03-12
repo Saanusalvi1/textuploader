15bdc95be2a8fc275f055117feb203770a174acf
Shankar <120117175+Shankarsalvi1@users.noreply.github.com>
2023-03-13 00:59:20 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,6 @@
-api_id = 9980982
-api_hash = "a6198445d05b755ec866c03b3358197b"
-bot_token = "5044521147:AAENtWyeVYrWsDwWu42QN96RpCyCE45f1Nc"
+api_id = 27005191
+api_hash = "25f3ca5a50a9eacee7c19441222d14c2"
+bot_token = "5659489542:AAGFjU4zc6fAasB4x1arOOqqOWpTpTCUe5M"
+auth_users = "5892324527"
+groups = "-1001782442011"
 
